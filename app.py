199e01f091e6149b28399c1c00698ccfbe88fe56
Atharv Chaudhari <61587515+Atharv-Chaudhari@users.noreply.github.com>
2022-02-07 15:10:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -30,11 +30,12 @@ def fun_all(self,code):
     options.add_argument('--disable-gpu')
     options.add_argument('--disable-dev-shm-usage')
     options.add_argument('--no-sandbox')
+    options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     options.add_experimental_option('excludeSwitches', ['enable-logging'])
 
     # code=input()
 
-    driver = webdriver.Chrome(executable_path='./chromedriver.exe',options=options)
+    driver = webdriver.Chrome(executable_path = os.environ.get("CHROMEDRIVER_PATH"),options=options)
     driver.get("https://lordsmobile.igg.com/gifts/")
     c=0
     data=pd.read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vStREOt4nVOvqHzL7M5JyW3OEbqXU5eH2o521cHRGE_8fiuSQpyzPKJXTf5queEFX_hu6XcLZfBYkAr/pub?output=csv")
@@ -82,11 +83,12 @@ def fun(self,code):
     options.add_argument('--disable-gpu')
     options.add_argument('--disable-dev-shm-usage')
     options.add_argument('--no-sandbox')
+    options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     options.add_experimental_option('excludeSwitches', ['enable-logging'])
 
     # code=input()
 
-    driver = webdriver.Chrome(executable_path='./chromedriver.exe',options=options)
+    driver = webdriver.Chrome(executable_path = os.environ.get("CHROMEDRIVER_PATH"),options=options)
     driver.get("https://lordsmobile.igg.com/gifts/")
     c=0
     data=pd.read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vTtW5iFJu3zb3TB07eptxo7JExb4xCMTGy-s4rnSzzrm2je0m_eQCZMTikCPQiluMrmWO77dMaPfJc8/pub?output=csv")
