a469da9824c6016e3e8470cff70ff37c2980b519
Atharv Chaudhari <61587515+Atharv-Chaudhari@users.noreply.github.com>
2022-02-07 12:12:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ def fun_all(code):
 
     # code=input()
 
-    driver = webdriver.Chrome(executable_path='https://github.com/Atharv-Chaudhari/Auto-Redeem-Codes-To-Multiple-Accounts-in-Single-Go-For-Lords-Mobile/blob/main/chromedriver.exe',options=options)
+    driver = webdriver.Chrome(executable_path='https://github.com/Atharv-Chaudhari/Auto-Redeem-Codes-To-Multiple-Accounts-in-Single-Go-For-Lords-Mobile/blob/main/chromedriver.exe?raw=true',options=options)
     driver.get("https://lordsmobile.igg.com/gifts/")
     c=0
     data=pd.read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vStREOt4nVOvqHzL7M5JyW3OEbqXU5eH2o521cHRGE_8fiuSQpyzPKJXTf5queEFX_hu6XcLZfBYkAr/pub?output=csv")
@@ -80,7 +80,7 @@ def fun(code):
 
     # code=input()
 
-    driver = webdriver.Chrome(executable_path='https://github.com/Atharv-Chaudhari/Auto-Redeem-Codes-To-Multiple-Accounts-in-Single-Go-For-Lords-Mobile/blob/main/chromedriver.exe',options=options)
+    driver = webdriver.Chrome(executable_path='https://github.com/Atharv-Chaudhari/Auto-Redeem-Codes-To-Multiple-Accounts-in-Single-Go-For-Lords-Mobile/blob/main/chromedriver.exe?raw=true',options=options)
     driver.get("https://lordsmobile.igg.com/gifts/")
     c=0
     data=pd.read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vTtW5iFJu3zb3TB07eptxo7JExb4xCMTGy-s4rnSzzrm2je0m_eQCZMTikCPQiluMrmWO77dMaPfJc8/pub?output=csv")
