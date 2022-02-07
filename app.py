c16bb07db3c4e61648b157e1d9404949d6a64906
Atharv Chaudhari <61587515+Atharv-Chaudhari@users.noreply.github.com>
2022-02-07 12:18:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,11 +24,12 @@ def fun_all(code):
     options.add_argument('--disable-gpu')
     options.add_argument('--disable-dev-shm-usage')
     options.add_argument('--no-sandbox')
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     options.add_experimental_option('excludeSwitches', ['enable-logging'])
 
     # code=input()
 
-    driver = webdriver.Chrome(executable_path='https://github.com/Atharv-Chaudhari/Auto-Redeem-Codes-To-Multiple-Accounts-in-Single-Go-For-Lords-Mobile/blob/main/chromedriver.exe?raw=true',options=options)
+    driver = webdriver.Chrome(executable_path = os.environ.get("CHROMEDRIVER_PATH"),options=options)
     driver.get("https://lordsmobile.igg.com/gifts/")
     c=0
     data=pd.read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vStREOt4nVOvqHzL7M5JyW3OEbqXU5eH2o521cHRGE_8fiuSQpyzPKJXTf5queEFX_hu6XcLZfBYkAr/pub?output=csv")
@@ -76,11 +77,12 @@ def fun(code):
     options.add_argument('--disable-gpu')
     options.add_argument('--disable-dev-shm-usage')
     options.add_argument('--no-sandbox')
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     options.add_experimental_option('excludeSwitches', ['enable-logging'])
 
     # code=input()
 
-    driver = webdriver.Chrome(executable_path='https://github.com/Atharv-Chaudhari/Auto-Redeem-Codes-To-Multiple-Accounts-in-Single-Go-For-Lords-Mobile/blob/main/chromedriver.exe?raw=true',options=options)
+    driver = webdriver.Chrome(executable_path = os.environ.get("CHROMEDRIVER_PATH"),options=options)
     driver.get("https://lordsmobile.igg.com/gifts/")
     c=0
     data=pd.read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vTtW5iFJu3zb3TB07eptxo7JExb4xCMTGy-s4rnSzzrm2je0m_eQCZMTikCPQiluMrmWO77dMaPfJc8/pub?output=csv")
