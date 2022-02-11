69193eb8c67ba4db68ea6a193fd98ceb177b64e6
Atharv Chaudhari <61587515+Atharv-Chaudhari@users.noreply.github.com>
2022-02-11 09:10:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import os
 
 fapp = Flask(__name__)
 
-fapp.config['CELERY_BROKER_URL'] = 'amqps://chomczcf:uYRWuvJe_rt8J88KpZO45_2nNUS2wYfI@gull.rmq.cloudamqp.com/chomczcf'
+fapp.config['CELERY_BROKER_URL'] = os.environ.get("cloudamqp")
 celery = Celery(fapp.name, broker=fapp.config['CELERY_BROKER_URL'])
 celery.conf.update(fapp.config)
 
@@ -41,12 +41,12 @@ def fun_all(self, code):
         "CHROMEDRIVER_PATH"), options=options)
     driver.get("https://lordsmobile.igg.com/gifts/")
     data = pd.read_csv(
-        "https://docs.google.com/spreadsheets/d/e/2PACX-1vStREOt4nVOvqHzL7M5JyW3OEbqXU5eH2o521cHRGE_8fiuSQpyzPKJXTf5queEFX_hu6XcLZfBYkAr/pub?output=csv")
+        os.environ.get("data_all"))
     for i in data["ID"]:
         # driver = webdriver.Chrome(executable_path='./chromedriver.exe',options=options)
         # driver.get("https://lordsmobile.igg.com/gifts/")
         element = driver.find_element(By.ID, "iggid")
-        element.send_keys(i)
+        element.send_keys(int(i))
         element = driver.find_element(By.ID, "cdkey_1")
         element.send_keys(code)
         element = driver.find_element(By.ID, "btn_claim_1").click()
@@ -91,12 +91,12 @@ def fun(self, code):
         "CHROMEDRIVER_PATH"), options=options)
     driver.get("https://lordsmobile.igg.com/gifts/")
     data = pd.read_csv(
-        "https://docs.google.com/spreadsheets/d/e/2PACX-1vTtW5iFJu3zb3TB07eptxo7JExb4xCMTGy-s4rnSzzrm2je0m_eQCZMTikCPQiluMrmWO77dMaPfJc8/pub?output=csv")
+        os.environ.get("data"))
     for i in data["ID"]:
         # driver = webdriver.Chrome(executable_path='./chromedriver.exe',options=options)
         # driver.get("https://lordsmobile.igg.com/gifts/")
         element = driver.find_element(By.ID, "iggid")
-        element.send_keys(i)
+        element.send_keys(int(i))
         element = driver.find_element(By.ID, "cdkey_1")
         element.send_keys(code)
         element = driver.find_element(By.ID, "btn_claim_1").click()
