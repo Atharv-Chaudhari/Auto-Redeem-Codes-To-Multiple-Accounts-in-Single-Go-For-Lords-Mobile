1f1749bfd599f7e58d77873cf9df30eb191676a6
Atharv Chaudhari <61587515+Atharv-Chaudhari@users.noreply.github.com>
2022-02-17 21:01:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,12 @@ from flask import Flask, render_template
 from httpx import request
 from celery import Celery
 import os
+import datetime
+from threading import Thread
+from selenium.webdriver.common.by import By
+import pandas as pd
+import time
+from selenium import webdriver
 
 fapp = Flask(__name__)
 
@@ -9,14 +15,34 @@ fapp.config['CELERY_BROKER_URL'] = os.environ.get("cloudamqp")
 celery = Celery(fapp.name, broker=fapp.config['CELERY_BROKER_URL'])
 celery.conf.update(fapp.config)
 
+check={}
 
 @celery.task(bind=True)
-def fun_all(self, code):
-    from selenium import webdriver
-    from selenium.webdriver.common.by import By
-    import pandas as pd
-    import time
+def load_task(self,code,data_url):
+    threads=[]
+    check={}
+    # print("Starting Process",datetime.datetime.now().strftime("%H:%M:%S"))
+    a = datetime.datetime.now()
+    data=pd.read_csv(data_url)
+    c=0
+    for i in data['ID']:
+        check[int(i)]=0
+        threads.append(Thread(target = fun, args = (code,int(i),check)))
+        c+=1
 
+    for i in range(c):
+        threads[i].start()
+    
+    for i in range(c):
+        threads[i].join()
+
+    # print("Ending Process",datetime.datetime.now().strftime("%H:%M:%S"))
+    b = datetime.datetime.now()
+    print("Total Time Taken :- ",b-a)
+    print(check)
+    return
+
+def fun(code,x,check):
     user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36"
 
     options = webdriver.ChromeOptions()
@@ -39,83 +65,24 @@ def fun_all(self, code):
 
     driver = webdriver.Chrome(executable_path=os.environ.get(
         "CHROMEDRIVER_PATH"), options=options)
-    driver.get("https://lordsmobile.igg.com/gifts/")
-    data = pd.read_csv(
-        os.environ.get("data_all"))
-    for i in data["ID"]:
-        try:
-            # driver = webdriver.Chrome(executable_path='./chromedriver.exe',options=options)
-            # driver.get("https://lordsmobile.igg.com/gifts/")
-            element = driver.find_element(By.ID, "iggid")
-            element.send_keys(int(i))
-            element = driver.find_element(By.ID, "cdkey_1")
-            element.send_keys(code)
-            element = driver.find_element(By.ID, "btn_claim_1").click()
-            element = driver.find_element(By.ID, "btn_msg_close").click()
-            print("Redeemption Attempt Completed in ID :-", int(i))
-            # driver.close()
-            driver.refresh()
-            time.sleep(5)
-        except Exception as e:
-            print(e)
-            continue
-
+    try:
+        driver.get("https://lordsmobile.igg.com/gifts/")
+    except Exception as e:
+        print(e)
+    try:
+        element = driver.find_element(By.ID, "iggid")
+        element.send_keys(x)
+        element = driver.find_element(By.ID, "cdkey_1")
+        element.send_keys(code)
+        element = driver.find_element(By.ID, "btn_claim_1").click()
+        element = driver.find_element(By.ID, "btn_msg_close").click()
+        print("Redeemption Attempt Completed in ID :-", x)
+        check[x]=1
+        driver.refresh()
+        time.sleep(2)
+    except Exception as e:
+        print(e)
     driver.close()
-    # return c
-
-
-@celery.task(bind=True)
-def fun(self, code):
-    from selenium import webdriver
-    from selenium.webdriver.common.by import By
-    import pandas as pd
-    import time
-
-    user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36"
-
-    options = webdriver.ChromeOptions()
-    options.headless = True
-    options.add_argument(f'user-agent={user_agent}')
-    options.add_argument("--window-size=1920,1080")
-    options.add_argument('--ignore-certificate-errors')
-    options.add_argument('--allow-running-insecure-content')
-    options.add_argument("--disable-extensions")
-    options.add_argument("--proxy-server='direct://'")
-    options.add_argument("--proxy-bypass-list=*")
-    options.add_argument("--start-maximized")
-    options.add_argument('--disable-gpu')
-    options.add_argument('--disable-dev-shm-usage')
-    options.add_argument('--no-sandbox')
-    options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-    options.add_experimental_option('excludeSwitches', ['enable-logging'])
-
-    # code=input()
-
-    driver = webdriver.Chrome(executable_path=os.environ.get(
-        "CHROMEDRIVER_PATH"), options=options)
-    driver.get("https://lordsmobile.igg.com/gifts/")
-    data = pd.read_csv(
-        os.environ.get("data"))
-    for i in data["ID"]:
-        try:
-            # driver = webdriver.Chrome(executable_path='./chromedriver.exe',options=options)
-            # driver.get("https://lordsmobile.igg.com/gifts/")
-            element = driver.find_element(By.ID, "iggid")
-            element.send_keys(int(i))
-            element = driver.find_element(By.ID, "cdkey_1")
-            element.send_keys(code)
-            element = driver.find_element(By.ID, "btn_claim_1").click()
-            element = driver.find_element(By.ID, "btn_msg_close").click()
-            print("Redeemption Attempt Completed in ID :-", int(i))
-            # driver.close()
-            driver.refresh()
-            time.sleep(5)
-        except Exception as e:
-            print(e)
-            continue
-
-    driver.close()
-    # return c
 
 
 @fapp.route('/', methods=['POST', 'GET'])
@@ -128,8 +95,8 @@ def redeem():
     from flask import request
     if request.method == 'POST':
         print("The Redeem Code :-", request.form.get('code'))
-        fun.delay(request.form.get('code'))
-        fun_all.delay(request.form.get('code'))
+        url1=request.form.get('url1')
+        load_task.delay(request.form.get('code'),url1)
         return render_template("task.html")
     else:
         return render_template("home.html")
